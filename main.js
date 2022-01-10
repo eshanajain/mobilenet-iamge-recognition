41d8134b2fa4500f35416d0e9d032e9163f11d95
eshanajain <82397896+eshanajain@users.noreply.github.com>
2022-01-10 16:32:43 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -29,7 +29,7 @@ function gotResult(error, results) {
       synth.speak(utterThis);
 
       document.getElementById("result_object_name").innerHTML = results[0].label;
-      document.getElementById("results_object_accuracy").innerHTML = results[0].confidence.toFixed(3);
+      document.getElementById("result_object_accuracy").innerHTML = results[0].confidence.toFixed(3);
     }
   }
 }
